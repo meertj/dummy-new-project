098c015dc223f01216cea243f994e8b1acbefbcf
Joe <38014449+meertj@users.noreply.github.com>
2023-06-04 22:13:03 -0400

    Add main for new project

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    // This is meant to represent a project that requires some utilities from an older project.
+    // Instead of copying them over or importing the project, we'd rather carry over the files and maintain the git history.
+}
